ab60812fbe172643605c8abce106b59365bdd93d
ilizin <ilio.zincone@gmx.com>
2025-03-06 16:54:24 +0100

    created README

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,17 @@
+CREATE TABLE advertisement (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(255),
+    city varchar(255),
+    country varchar(255),
+    address varchar(255),
+    price bigint,
+    description varchar(255)
+);
+
+CREATE TABLE advertiser (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255)
+);
 
 --INSERT INTO property (title, city, country, address, price, description) VALUES
   --('test_title1', 'test_city1', 'test_country1', 'test_address1', 'test_price1', 'test_description1'),
