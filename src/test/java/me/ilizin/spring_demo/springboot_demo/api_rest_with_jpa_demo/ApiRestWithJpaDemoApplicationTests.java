b7da1c8299cb51325837ccc8f8ab2bf400e47f2b
ilizin <ilio.zincone@gmx.com>
2025-03-09 19:45:34 +0100

    added validation to check the controllers are instantiated

--- src/test/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/ApiRestWithJpaDemoApplicationTests.java
+++ src/test/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/ApiRestWithJpaDemoApplicationTests.java
@@ -1,13 +1,25 @@
 package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo;
 
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.rest.AdvertisementRestController;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.rest.AdvertiserRestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiRestWithJpaDemoApplicationTests {
 
+	@Autowired
+	private AdvertiserRestController advertiserRestController;
+	@Autowired
+	private AdvertisementRestController advertisementRestController;
+
 	@Test
 	void contextLoads() {
+		assertThat(advertiserRestController).isNotNull();
+		assertThat(advertisementRestController).isNotNull();
 	}
 
 }
