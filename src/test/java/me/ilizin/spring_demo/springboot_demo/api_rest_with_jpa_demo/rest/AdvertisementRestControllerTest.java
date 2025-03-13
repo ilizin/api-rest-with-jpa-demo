77283eaabd232f81c8587e3c93d01797c31f8f62
ilizin <ilio.zincone@gmx.com>
2025-03-13 21:41:07 +0100

    added morevalidations

--- src/test/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/rest/AdvertisementRestControllerTest.java
+++ src/test/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/rest/AdvertisementRestControllerTest.java
@@ -2,11 +2,13 @@ package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.rest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.entity.Advertisement;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
 
 /* Note the use of webEnvironment=RANDOM_PORT to start the server with a random port (useful to avoid conflicts
    in test environments) and the injection of the port with @LocalServerPort. Also, note that Spring Boot has automatically
@@ -23,5 +25,9 @@ public class AdvertisementRestControllerTest {
     @Test
     void greetingShouldReturnDefaultMessage() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/advertisements", String.class)).contains("[]");
+        Advertisement advertisement = new Advertisement("title", "city", "country", "address", 1000, "description");
+        HttpEntity<Advertisement> request = new HttpEntity<>(advertisement);
+        assertThat(this.restTemplate.postForEntity("http://localhost:" + port + "/api/advertisements", request, String.class)).isNotNull();
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/api/advertisements", String.class)).contains("[{\"id\":1,\"title\":\"title\",\"city\":\"city\",\"country\":\"country\",\"address\":\"address\",\"price\":1000,\"description\":\"description\"}]");
     }
 }
