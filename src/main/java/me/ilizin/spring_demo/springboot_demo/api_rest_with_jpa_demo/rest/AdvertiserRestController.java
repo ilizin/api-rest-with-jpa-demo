ca33f7d1689924a98615b43fa0d313e0fcb5298a
ilizin <ilio.zincone@gmx.com>
2025-07-18 21:53:04 +0200

    Added some info to the swagger

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/rest/AdvertiserRestController.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/rest/AdvertiserRestController.java
@@ -1,6 +1,10 @@
 package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.rest;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.entity.Advertiser;
 import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.service.AdvertiserService;
@@ -20,12 +24,18 @@ public class AdvertiserRestController {
     }
 
     @Operation(summary = "Get all the advertisers")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved some data")
+    })
     @GetMapping("/advertisers")
     public List<Advertiser> findAll() {
         return advertiserService.findAll();
     }
 
     @Operation(summary = "Find an advertiser by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved some data")
+    })
     @GetMapping("/advertiser/{advertiserId}")
     public Advertiser getAdvertiser(@PathVariable int advertiserId) {
         Advertiser advertiser = advertiserService.findById(advertiserId);
@@ -36,6 +46,9 @@ public class AdvertiserRestController {
     }
 
     @Operation(summary = "Add a new advertiser")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully added a new advertiser")
+    })
     @PostMapping("/advertiser")
     public Advertiser addAdvertiser(@RequestBody Advertiser advertiser) {
         // Just in case they pass an id in JSON, set the id to 0, to force a save of new item instead of update.
@@ -44,12 +57,18 @@ public class AdvertiserRestController {
     }
 
     @Operation(summary = "Update an existing advertiser")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully updated an existing advertiser")
+    })
     @PutMapping("/advertiser")
     public Advertiser updateAdvertiser(@RequestBody Advertiser advertiser) {
         return advertiserService.save(advertiser);
     }
 
     @Operation(summary = "Delete an existing advertiser")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted an existing advertiser")
+    })
     @DeleteMapping("/advertiser/{advertiserId}")
     public void deleteAdvertiser(@PathVariable int advertiserId) {
         Advertiser advertiser = advertiserService.findById(advertiserId);
