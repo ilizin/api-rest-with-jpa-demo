52d0fd778a1319344d1bb0b57b0c3eefcfea2b16
ilizin <ilio.zincone@gmx.com>
2026-01-31 23:29:24 +0100

    Progressing with the car controller implementation (4)

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/model/dto/CarInDto.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/model/dto/CarInDto.java
@@ -1,4 +1,99 @@
 package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.model.dto;
 
+import jakarta.persistence.Column;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.enums.BodyType;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.enums.FuelType;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.enums.GearBox;
+
 public class CarInDto {
+
+    private String make;
+
+    private String model;
+
+    private int firstRegistrationFrom;
+
+    private BodyType bodyType;
+
+    private int price;
+
+    private int mileage;
+
+    private FuelType fuelType;
+
+    private GearBox gearBox;
+
+    private int power;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getFirstRegistrationFrom() {
+        return firstRegistrationFrom;
+    }
+
+    public void setFirstRegistrationFrom(int firstRegistrationFrom) {
+        this.firstRegistrationFrom = firstRegistrationFrom;
+    }
+
+    public BodyType getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(BodyType bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(FuelType fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public GearBox getGearBox() {
+        return gearBox;
+    }
+
+    public void setGearBox(GearBox gearBox) {
+        this.gearBox = gearBox;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
 }
--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/model/entity/Car.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/model/entity/Car.java
@@ -1,7 +1,6 @@
 package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.model.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.enums.BodyType;
 import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.enums.FuelType;
 import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.enums.GearBox;
@@ -9,13 +8,116 @@ import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.enums.GearBo
 @Entity
 @Table(name="car")
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="make")
     private String make;
+
+    @Column(name="model")
     private String model;
+
+    @Column(name="firstRegistrationFrom")
     private int firstRegistrationFrom;
+
+    @Column(name="bodyType")
     private BodyType bodyType;
+
+    @Column(name="price")
     private int price;
+
+    @Column(name="mileage")
     private int mileage;
+
+    @Column(name="fuelType")
     private FuelType fuelType;
+
+    @Column(name="gearBox")
     private GearBox gearBox;
+
+    @Column(name="power")
     private int power;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getFirstRegistrationFrom() {
+        return firstRegistrationFrom;
+    }
+
+    public void setFirstRegistrationFrom(int firstRegistrationFrom) {
+        this.firstRegistrationFrom = firstRegistrationFrom;
+    }
+
+    public BodyType getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(BodyType bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(FuelType fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public GearBox getGearBox() {
+        return gearBox;
+    }
+
+    public void setGearBox(GearBox gearBox) {
+        this.gearBox = gearBox;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
 }
