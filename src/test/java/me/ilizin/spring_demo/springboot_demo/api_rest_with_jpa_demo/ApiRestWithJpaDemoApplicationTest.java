469818dac20b0ea5d4669e4376589b82438c4269
ilizin <ilio.zincone@gmx.com>
2026-02-04 21:12:15 +0100

    Progressing with junit

--- src/test/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/ApiRestWithJpaDemoApplicationTest.java
+++ src/test/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/ApiRestWithJpaDemoApplicationTest.java
@@ -1,6 +1,9 @@
 package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo;
 
 import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.controllers.CarRestController;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.controllers.MotorcycleRestController;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.services.ICarService;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.services.IMotorcycleService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,11 +16,17 @@ class ApiRestWithJpaDemoApplicationTest {
 	@Autowired
 	private CarRestController carRestController;
 	@Autowired
-	private AdvertiserService advertiserService;
+	private MotorcycleRestController motorcycleRestController;
+	@Autowired
+	private ICarService carService;
+	@Autowired
+	private IMotorcycleService motorcycleService;
 
 	@Test
 	void contextLoads() {
 		assertThat(carRestController).isNotNull();
-		assertThat(advertiserService).isNotNull();
+		assertThat(motorcycleRestController).isNotNull();
+		assertThat(carService).isNotNull();
+		assertThat(motorcycleService).isNotNull();
 	}
 }
