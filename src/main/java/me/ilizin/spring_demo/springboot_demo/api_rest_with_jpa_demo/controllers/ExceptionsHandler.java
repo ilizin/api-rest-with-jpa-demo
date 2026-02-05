8a5c423a456f16832943c4ab2843589b785fc542
ilizin <ilio.zincone@gmx.com>
2026-02-05 21:39:11 +0100

    Delete old controller code

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/controllers/ExceptionsHandler.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/controllers/ExceptionsHandler.java
@@ -1,7 +1,8 @@
 package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.controllers;
 
-import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.exceptions.AdvertiserNotFoundException;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.exceptions.CarNotFoundException;
 import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.exceptions.ErrorResponse;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.exceptions.MotorcycleNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,7 +12,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ExceptionsHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(AdvertiserNotFoundException exception) {
+    public ResponseEntity<ErrorResponse> handleException(CarNotFoundException exception) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(MotorcycleNotFoundException exception) {
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
         errorResponse.setMessage(exception.getMessage());
