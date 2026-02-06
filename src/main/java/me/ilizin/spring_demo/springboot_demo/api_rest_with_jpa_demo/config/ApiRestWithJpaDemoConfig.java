59ee9dcb52df8d40a6429b3bb757a3df354886fb
ilizin <ilio.zincone@gmx.com>
2026-02-06 22:44:40 +0100

    Add more info into the swagger

---
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/config/ApiRestWithJpaDemoConfig.java
@@ -0,0 +1,31 @@
+package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiRestWithJpaDemoConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info()
+                        .version("1.0.0-SNAPSHOT")
+                        .title("Api rest with JPA demo")
+                        .description("A CRUD API rest with Jpa/hibernate (h2) demo project")
+                        .contact(new Contact()
+                                .name("Contact the developer")
+                                .url("https://github.com/ilizin"))
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Find out more about Swagger")
+                        .url("https://swagger.io/"));
+    }
+}
