aba04a7385afb164ed06b87f09351bb67b64a868
ilizin <ilio.zincone@gmx.com>
2026-02-12 22:57:08 +0100

    Do not extend from Vehicle class

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/model/entity/Motorcycle.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/model/entity/Motorcycle.java
@@ -4,22 +4,103 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name="motorcycle")
-public class Motorcycle extends Vehicle {
+public class Motorcycle {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
 
+    @Column(name="make")
+    private String make;
+
+    @Column(name="model")
+    private String model;
+
+    @Column(name="firstRegistrationFrom")
+    private int firstRegistrationFrom;
+
+    @Column(name="price")
+    private int price;
+
+    @Column(name="mileage")
+    private int mileage;
+
+    @Column(name="fuelType")
+    private String fuelType;
+
+    @Column(name="gearBox")
+    private String gearBox;
+
+    @Column(name="power")
+    private int power;
+
     @Column(name="bodyType")
     private String bodyTypeMotorcycle;
 
-    public String getBodyType() {
-        return bodyTypeMotorcycle;
+
+    public String getMake() {
+        return make;
     }
 
-    public void setBodyTypeMotorcycle(String bodyTypeMotorcycle) {
-        this.bodyTypeMotorcycle = bodyTypeMotorcycle;
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getFirstRegistrationFrom() {
+        return firstRegistrationFrom;
+    }
+
+    public void setFirstRegistrationFrom(int firstRegistrationFrom) {
+        this.firstRegistrationFrom = firstRegistrationFrom;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public String getGearBox() {
+        return gearBox;
+    }
+
+    public void setGearBox(String gearBox) {
+        this.gearBox = gearBox;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
     }
 
     public int getId() {
@@ -29,4 +110,13 @@ public class Motorcycle extends Vehicle {
     public void setId(int id) {
         this.id = id;
     }
+
+    public String getBodyType() {
+        return bodyTypeMotorcycle;
+    }
+
+    public void setBodyTypeMotorcycle(String bodyTypeMotorcycle) {
+        this.bodyTypeMotorcycle = bodyTypeMotorcycle;
+    }
+
 }
