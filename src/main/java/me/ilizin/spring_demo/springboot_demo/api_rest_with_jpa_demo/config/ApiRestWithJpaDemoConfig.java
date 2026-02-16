4563e1d33b168591f94ee75d6e4965daad662230
ilizin <ilio.zincone@gmx.com>
2026-02-16 21:35:48 +0100

    Improve yaml

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/config/ApiRestWithJpaDemoConfig.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/config/ApiRestWithJpaDemoConfig.java
@@ -1,10 +1,6 @@
 package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.config;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,24 +22,6 @@ public class ApiRestWithJpaDemoConfig implements WebMvcConfigurer {
     @Autowired
     private BuildProperties buildProperties;
 
- /*   @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new Info()
-                        .version(buildProperties.getVersion())
-                        .title(buildProperties.getName())
-                        .description("A CRUD API rest with Jpa/hibernate (h2) demo project")
-                        .contact(new Contact()
-                                .name("Contact the developer")
-                                .url("https://github.com/ilizin"))
-                        .termsOfService("http://swagger.io/terms/")
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Find out more about Swagger")
-                        .url("https://swagger.io/"));
-    }*/
-
     @Bean
     public OpenAPI customOpenAPI() throws IOException {
         ClassPathResource resource = new ClassPathResource("api-rest-with-jpa-demo-swagger.yaml");
@@ -51,7 +29,10 @@ public class ApiRestWithJpaDemoConfig implements WebMvcConfigurer {
         String content = new String(Files.readAllBytes(path));
         SwaggerParseResult result = new OpenAPIV3Parser().readContents(content);
         if (result.getMessages().isEmpty() && result.getOpenAPI() != null) {
-            return result.getOpenAPI();
+            OpenAPI openAPI = result.getOpenAPI();
+            openAPI.getInfo().setTitle(buildProperties.getName());
+            openAPI.getInfo().setVersion(buildProperties.getVersion());
+            return openAPI;
         } else {
             throw new RuntimeException("Failed to parse OpenAPI definition: " + result.getMessages());
         }
