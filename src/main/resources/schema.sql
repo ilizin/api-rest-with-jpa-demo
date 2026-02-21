e35dd3e447c9e0b58645f972d013ab35ca23268b
ilizin <ilio.zincone@gmx.com>
2026-02-21 17:28:02 +0100

    Fix output type, add cars into the database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -23,3 +23,9 @@ CREATE TABLE IF NOT EXISTS motorcycle (
     gear_box varchar(255),
     power integer
 );
+
+INSERT INTO car VALUES (1, 'Lamborghini', 'Miura', 1970, 'COUPE', 2700000, 48000, 'GASOLINE', 'MANUAL', 380);
+INSERT INTO car VALUES (2, 'Ferrari', 'Testarossa', 1984, 'COUPE', 250000, 51000, 'GASOLINE', 'MANUAL', 380);
+INSERT INTO car VALUES (3, 'Citroën', 'XM', 1992, 'SEDAN', 13900, 75500, 'GASOLINE', 'MANUAL', 194);
+INSERT INTO car VALUES (4, 'Ferrari', 'Enzo', 2003, 'COUPE', 4800000, 32000, 'GASOLINE', 'MANUAL', 650);
+INSERT INTO car VALUES (5, 'Lamborghini', 'Countach', 1981, 'COUPE', 700000, 73000, 'GASOLINE', 'MANUAL', 455);
