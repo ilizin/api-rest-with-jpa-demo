f33080bc9f953a5ed62b72e7e07cfd33d6e6dfe7
ilizin <ilio.zincone@gmx.com>
2026-03-03 23:26:42 +0100

    Inserta a motorcycle example

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -32,8 +32,4 @@ CREATE TABLE IF NOT EXISTS motorcycle (
     power integer
 );
 
-INSERT INTO motorcycle (make, model, submodel, first_registration_from, body_type, price, mileage, fuel_type, gear_box, power) VALUES ('Lamborghini', 'Miura', null, 1970, 'COUPE', 2700000, 48000, 'GASOLINE', 'MANUAL', 380);
-INSERT INTO motorcycle (make, model, submodel, first_registration_from, body_type, price, mileage, fuel_type, gear_box, power) VALUES ('Ferrari', 'Testarossa', null, 1984, 'COUPE', 250000, 51000, 'GASOLINE', 'MANUAL', 380);
-INSERT INTO motorcycle (make, model, submodel, first_registration_from, body_type, price, mileage, fuel_type, gear_box, power) VALUES ('Citroën', 'XM', null, 1992, 'SEDAN', 13900, 75500, 'GASOLINE', 'MANUAL', 194);
-INSERT INTO motorcycle (make, model, submodel, first_registration_from, body_type, price, mileage, fuel_type, gear_box, power) VALUES ('Ferrari', 'Enzo', null, 2003, 'COUPE', 4800000, 32000, 'GASOLINE', 'MANUAL', 650);
-INSERT INTO motorcycle (make, model, submodel, first_registration_from, body_type, price, mileage, fuel_type, gear_box, power) VALUES ('Lamborghini', 'Countach', null, 1981, 'COUPE', 700000, 73000, 'GASOLINE', 'MANUAL', 455);
+INSERT INTO motorcycle (make, model, submodel, first_registration_from, body_type, price, mileage, fuel_type, gear_box, power) VALUES ('Ducati', '620 Sport', null, 2004, 'SUPERSPORT', 2700, 20000 'GASOLINE', 'MANUAL', 60);
