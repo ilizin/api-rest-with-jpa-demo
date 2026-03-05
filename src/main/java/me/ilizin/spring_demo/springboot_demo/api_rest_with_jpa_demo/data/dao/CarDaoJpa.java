b6f72f0872ce1b2590d70640242f764b14f3f2c6
ilizin <ilio.zincone@gmx.com>
2026-03-05 21:50:03 +0100

    Progress with vehicle fix error (2)

--- src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/data/dao/CarDaoJpa.java
+++ src/main/java/me/ilizin/spring_demo/springboot_demo/api_rest_with_jpa_demo/data/dao/CarDaoJpa.java
@@ -2,6 +2,7 @@ package me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.data.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
+import me.ilizin.spring_demo.springboot_demo.api_rest_with_jpa_demo.model.entity.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -18,21 +19,21 @@ public class CarDaoJpa implements ICarDao {
     }
 
     @Override
-    public List<Car> findAll() {
-        TypedQuery<Car> query = entityManager.createQuery("from Car", Car.class);
+    public List<Vehicle> findAll() {
+        TypedQuery<Vehicle> query = entityManager.createQuery("from Car", Vehicle.class);
         return query.getResultList();
     }
 
     @Override
-    public Car findById(int id) {
-        return entityManager.find(Car.class, id);
+    public Vehicle findById(int id) {
+        return entityManager.find(Vehicle.class, id);
     }
 
     /* We don't use @Transactional at DAO layer, it will be handled at service layer. When you're making use of DAOs
        and services together, the best practice is that, the DAO does not handle the transactional, instead, it's managed
        at the service layer. */
     @Override
-    public Car save(Car car) {
+    public Vehicle save(Vehicle car) {
         /* This save or update depending on the property id, if the id == 0 then insert/save else update
            Return the db property, it has updated id from the database (in case of insert) */
         return entityManager.merge(car);
@@ -40,7 +41,7 @@ public class CarDaoJpa implements ICarDao {
 
     @Override
     public void deleteById(int id) {
-        Car car = findById(id);
+        Vehicle car = findById(id);
         entityManager.remove(car);
     }
 }
